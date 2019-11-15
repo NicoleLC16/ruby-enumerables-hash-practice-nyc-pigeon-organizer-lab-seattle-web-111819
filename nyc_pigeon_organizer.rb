09de82140e4d3f2339da5c12199a074d6e2dd7f5
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-15 04:44:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,9 @@
 def nyc_pigeon_organizer(data)
 pigeon_hash = {}
-  data.each |categories, subcategories| do
-    subcategories.each do |names, value|
-      pigeon_hash[name] ||= {}
-      pigeon_hash[name][subcategories] ||= []
-      pigeon_hash[name][subcategories] << value
-  end
-  pigeon_hash
+
+data.each do |categories, subcategories|
+  subcategories.each ||
+
+end 
+pigeon_hash
 end
